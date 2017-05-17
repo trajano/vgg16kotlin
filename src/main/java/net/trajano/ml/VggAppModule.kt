d894c291b5ab3fefa5755b1eca64f78be843013a
Archimedes Trajano <developer@trajano.net>
2017-05-17 16:03:39 -0400

    Cleaned up code

--- src/main/java/net/trajano/ml/VggAppModule.kt
+++ src/main/java/net/trajano/ml/VggAppModule.kt
@@ -4,54 +4,18 @@ import dagger.Binds
 import dagger.Component
 import dagger.Module
 import dagger.Provides
-
-/**
- * Created by trajano on 2017-05-17.
- */
-//@Module(includes = arrayOf(VggAppBindings::class))
-//class VggAppModule {
-//}
-//
-//@Module
-//interface VggAppBindings {
-//    @Binds
-//    fun provideFileProvider(p: DefaultFileProvider): FileProvider
-//}
-
-//@Module
-//interface VggAppModule {
-//    @Binds
-//    fun provideFileProvider(p: DefaultFileProvider): FileProvider
-//}
-
-//abstract class VggAppModule {
-//    @Binds
-//    abstract fun provideFileProvider(p: DefaultFileProvider): FileProvider
-//}
+import javax.inject.Singleton
 
 @Module
 class VggAppModule {
 
-    @Provides
+    @Singleton @Provides
     fun provideFileProvider(): FileProvider {
         return DefaultFileProvider()
     }
 
-    @Provides
+    @Singleton @Provides
     fun provideNetworkProvider(p: VggNetworkProvider): NetworkProvider {
         return p
     }
-
-//    @Provides
-//    fun provideFileProvider(p: DefaultFileProvider): FileProvider {
-//        return p
-//    }
-
-//    @Provides
-//    fun provideNetworkProvider(): NetworkProvider {
-//        return VggNetworkProvider()
-//    }
-
-    //@Binds
-    //fun provideNetworkProvider(p: VggNetworkProvider): NetworkProvider
 }
